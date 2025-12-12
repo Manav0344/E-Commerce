aeca706f7af1e958dc3b5519f681210c103e4089
Manav Solanki <manavsolanki344@gmail.com>
2025-12-12 11:04:36 +0530

    Fix router for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,39 @@
-
 import './App.css'
-import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom';
+import { createHashRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Header from './common/Header'
 import Footer from './common/Footer';
 import Home from './Components/Home';
-import ProductList from './Components/ProductList';
 import ProductDetails from './Components/ProductDetails';
 
 const Layout = () => {
-    return (
-      <>
-        <Header />
-        <Outlet />
-        <Footer/>
-      </>
-    )
-  }
-
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 function App() {
-
-  
-
-
-  const router = createBrowserRouter([
-
-    
+  const router = createHashRouter([
     {
-       path: '/',
-       element: <Layout />,
-       children: [
+      path: '/',
+      element: <Layout />,
+      children: [
         {
           path: '/',
           element: <Home />
         },
-
-        // {
-        //   path: '/products',
-        //   element: <ProductList />
-        // },
-
         {
-          path : "/product/:id",
+          path: '/product/:id',
           element: <ProductDetails />
         }
-       ]
-       
-        
+      ]
     }
-  ])
+  ]);
 
-  return (
-    <>
-      <RouterProvider router= {router} />
-      
-      
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
